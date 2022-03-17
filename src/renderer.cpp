b8a4644e7f5b272cb65a12813053062c5c7521de
Athos <Athosvk@users.noreply.github.com>
2022-03-17 13:59:39 +0100

    Fixed compilation issue

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -6,13 +6,13 @@ namespace phyl{
 	Renderer::Renderer(Options *_options)
 	: options{_options} {
 		InitWindow(options->getInt("screen_width", 800), options->getInt("screen_height", 450), "Phylip");
-		camera.position = (Vector3){options->getFloat("cam_pos_x", 50.0f),
+		camera.position = Vector3{options->getFloat("cam_pos_x", 50.0f),
 									options->getFloat("cam_pos_y", 50.0f),
 									options->getFloat("cam_pos_z", 50.0f)};
-		camera.target = (Vector3){options->getFloat("cam_dir_x", 0.0f),
+		camera.target = Vector3{options->getFloat("cam_dir_x", 0.0f),
 									options->getFloat("cam_dir_y", 10.0f),
 									options->getFloat("cam_dir_z", 0.0f)};
-		camera.up = (Vector3){options->getFloat("cam_up_x", 0.0f),
+		camera.up = Vector3{options->getFloat("cam_up_x", 0.0f),
 									options->getFloat("cam_up_y", 1.0f),
 									options->getFloat("cam_up_z", 0.0f)};
 		camera.fovy = options->getFloat("cam_fov", 90.0f);
