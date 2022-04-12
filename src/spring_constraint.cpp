2ae07b3a5a500bb799fec06c7c18e6f356978a9c
Athos <Athosvk@users.noreply.github.com>
2022-04-12 13:28:56 +0200

    Added spring energy

--- src/spring_constraint.cpp
+++ src/spring_constraint.cpp
@@ -11,8 +11,10 @@ namespace phyl
 		return stiffness * (x_ij.norm() - RestLength) * x_ij.normalized();
 	}
 
-	double SpringConstraint::EvaluatePotentialEnergy(const Eigen::VectorXd& evaluationPositions) const
+	double SpringConstraint::EvaluatePotentialEnergy(const Eigen::VectorXd& evaluationPositions, double stiffness) const
 	{
-		return 0.0;
+		Eigen::Vector3d difference = evaluationPositions.segment<3>(StartVertex) - evaluationPositions.segment<3>(EndVertex);
+		double stretch = difference.norm() - RestLength;
+		return 0.5 * stiffness * stretch * stretch;
 	}
 }
